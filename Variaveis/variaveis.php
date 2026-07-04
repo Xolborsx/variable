3ec4f792b8cb4bd022a26d897d0764fd8d96286e
Xolborsx <lucasmaurent91@gmail.com>
2026-07-04 15:38:31 -0300

    Add files via upload

---
+++ Variaveis/variaveis.php
@@ -0,0 +1,72 @@
+<?php
+
+echo "andre";
+echo "<hr>";
+
+//vairaveis string
+
+
+$nome = "Mathew";
+echo "My name is              :  " .$nome;
+
+//variaveis numericas 
+echo"<hr>";
+
+$idade = 20;
+
+     echo " my name is: " .$nome. " and my age is ".$idade;
+
+echo "<hr>";
+
+echo "1 plus 1 equals= <br>";
+
+$n1 = 1;
+
+$n2 = 1;
+
+$soma = $n1 + $n2;
+
+echo "$soma";
+
+echo"<hr>";
+
+// subtracão
+echo "do you know what is 10 minus 3? It is: <br> ";
+
+$n3 = 10;
+$n4 = 3;
+
+$sub = $n3 - $n4;
+
+echo"$sub";
+
+echo"<hr>";
+
+
+
+
+
+
+
+
+
+//multiplicacao
+Echo"do you know what is 5 times 10? it is: <br>";
+$n5 = 5;
+$n6 = 10;
+
+$multi = 5 * 10;
+
+echo "$multi";
+echo"<hr>";
+
+
+//divisao
+echo " do you know what is 100 divided by 4? it is: <br>";
+
+$n7 = 100;
+$n8 = 4;
+
+$div = $n7 / $n8;
+
+echo"$div";
